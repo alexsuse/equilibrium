e5333a46d4519ad43f14d113be0da4e6ae809e31
Alex Suse <alexsusemihl@gmail.com>
2012-05-29 17:05:56 +0200

    once more

--- plotting.py
+++ plotting.py
@@ -10,7 +10,7 @@ matplotlib.rc('text',usetex = True)
 #matplotlib.rc('mathtext',default='sf')
 matplotlib.rc('font',size=8.0)
 
-filetype='.eps'
+filetype='.png'
 
 try:
 	picklename
@@ -59,13 +59,13 @@ axplot = figmap.add_subplot(1,2,2)
 axplot.set_xlabel(r'$\alpha$')
 #axplot.set_ylabel(r'$MMSE/\sigma^2_x$')
 axplot.set_title(r'$MMSE/\sigma^2_x$ (simulated vs. mean-field)')
-axplot.plot(alphas,normsigma[:,5],'g',alphas,normsigma[:,10],'b',alphas,normsigma[:,15],'r',alphas,normmf[:,5],'g:',alphas,normmf[:,10],'b:',alphas,normmf[:,15],'r:',rasterized=True)
-axplot.legend((r'$\phi=0.5$',r'$\phi=1.0$',r'$\phi=1.5$'))
-min5 = np.array(np.where(normmf[:,5]==np.min(normmf[:,5]))).ravel()
+axplot.plot(alphas,normsigma[:,10],'b',alphas,normsigma[:,15],'r',alphas,normsigma[:,20],'g',alphas,normmf[:,20],'g:',alphas,normmf[:,10],'b:',alphas,normmf[:,15],'r:',rasterized=True)
+axplot.legend((r'$\phi=1.0$',r'$\phi=1.5$',r'$\phi=2.0$'))
+min20 = np.array(np.where(normmf[:,20]==np.min(normmf[:,20]))).ravel()
 min10 = np.array(np.where(normmf[:,10]==np.min(normmf[:,10]))).ravel()
 min15 = np.array(np.where(normmf[:,15]==np.min(normmf[:,15]))).ravel()
-axplot.plot(alphas[min5],normmf[min5,5],'go',alphas[min10],normmf[min10,10],'bo',alphas[min15],normmf[min15,15],'ro',rasterized=True)
-figmap.savefig(prefix+'_simulated'+filetype)
+axplot.plot(alphas[min10],normmf[min10,10],'bo',alphas[min15],normmf[min15,15],'ro',alphas[min20],normmf[min20,20],'go',rasterized=True)
+figmap.savefig(prefix+'_simulated'+filetype,dpi=200)
 plt.close(figmap)
 
 figmap = plt.figure()
@@ -78,7 +78,7 @@ axmap.set_ylabel(r'$\phi$')
 axmap.set_title(r'$MMSE/\sigma^2_x$')
 cb = plt.colorbar(immap)
 cb.set_label(r'$MMSE/\sigma^2_x$')
-figmap.savefig(prefix+'_mf'+filetype)
+figmap.savefig(prefix+'_mf'+filetype,dpi=200)
 plt.close(figmap)
 
 err = abs(sigmas-sigmasmf)/sigmas
@@ -93,6 +93,6 @@ axmap.set_ylabel(r'$\phi$')
 axmap.set_title(r'$MMSE/\sigma^2_x$')
 cb = plt.colorbar(immap)
 cb.set_label(r'$MMSE/\sigma^2_x$')
-figmap.savefig(prefix+'_error'+filetype)
+figmap.savefig(prefix+'_error'+filetype,dpi=200)
 plt.close(figmap)
 
